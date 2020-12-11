12ddfa999416b2105fe703699cc9fbac4b4913f8
Tusgavin <dias.gdo@gmail.com>
2020-12-11 19:17:34 -0300

    Add dependencies for Navigation

--- GithubStatus/src/routes/index.js
+++ GithubStatus/src/routes/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 
-const {Screen, Navigator} = createStackNavigator();
-
 import Guto from '../pages/guto';
 import Nutka from '../pages/nutka';
 
+const {Screen, Navigator} = createStackNavigator();
+
 export default () => {
   return (
-    <Navigator>
+    <Navigator screenOptions={{headerShown: false}}>
       <Screen name="Guto" component={Guto} />
       <Screen name="Nutka" component={Nutka} />
     </Navigator>
